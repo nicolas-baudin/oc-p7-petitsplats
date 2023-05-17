c953214e5e255625a5ef720612cf2552dd717240
Nicolas Baudin <baudin.nicolas@pm.me>
2023-05-17 17:58:39 +0200

    Removed filterTags from searchengine.js

--- scripts/utils/searchengine.js
+++ scripts/utils/searchengine.js
@@ -24,34 +24,3 @@ function searchEngine() {
     });
     displayRecipes(filteredRecipes);
 }
-
-function filterTags(recipe) {
-    let {ingredients, appliance, utensils} = recipe;
-    let allTagsOk = true;
-    let recipeIng = [];
-    if (ingTags.length !== 0) {
-        ingredients.forEach((e) => {
-            recipeIng.push(e.ingredient);
-        });
-        ingTags.forEach((tag) => {
-            if (!recipeIng.includes(tag)) {
-                allTagsOk = false;
-            }
-        });
-    } else if (appTags.length !== 0) {
-        appTags.forEach((tag) => {
-            if (tag != appliance) {
-                allTagsOk = false;
-            }
-        });
-    } else if (uteTags.length !== 0) {
-        uteTags.forEach((tag) => {
-            if (!utensils.includes(tag)) {
-                allTagsOk = false;
-            }
-        });
-    }
-    if (allTagsOk) {
-        filteredRecipes.push(recipe);
-    }
-}
