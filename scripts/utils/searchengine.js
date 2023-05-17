4970d28bd06e44723296fa5e96a1c79e7fe512c1
Nicolas Baudin <baudin.nicolas@pm.me>
2023-05-17 13:38:20 +0200

    Searchengine using foreach

--- scripts/utils/searchengine.js
+++ scripts/utils/searchengine.js
@@ -7,22 +7,21 @@ let filteredRecipes = [];
 
 function searchEngine() {
     filteredRecipes = [];
-    for (let i = 0; i < recipes.length; i++) {
-        let recipe = recipes[i];
+    recipes.forEach((recipe) => {
         let {name, ingredients, description} = recipe;
         let title = name.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "");
         let desc = description.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "");
         if (!searchbarValue || title.includes(searchbarValue) || desc.includes(searchbarValue)) {
             filterTags(recipe);
         } else {
-            for (let i = 0; i < ingredients.length; i++) {
-                let ing = ingredients[i].ingredient.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "");
+            ingredients.forEach((e) => {
+                let ing = e.ingredient.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "");
                 if (ing.includes(searchbarValue)) {
                     filterTags(recipe);
                 }
-            }
+            });
         }
-    }
+    });
     displayRecipes(filteredRecipes);
 }
 
