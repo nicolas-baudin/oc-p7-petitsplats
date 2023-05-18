b65b99597225872f6135a932e393d196a467694a
Nicolas Baudin <baudin.nicolas@pm.me>
2023-05-18 20:29:27 +0200

    Some fixes to avoid some loops

--- scripts/utils/searchengine.js
+++ scripts/utils/searchengine.js
@@ -1,17 +1,19 @@
 function searchEngine() {
     filteredRecipes = [];
+    allTags = ingTags.concat(appTags, uteTags);
     for (let i = 0; i < recipes.length; i++) {
         let recipe = recipes[i];
         let {name, ingredients, description} = recipe;
-        let title = name.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "");
-        let desc = description.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "");
+        let title = completeNormalize(name);
+        let desc = completeNormalize(description);
         if (!searchbarValue || title.includes(searchbarValue) || desc.includes(searchbarValue)) {
             filterTags(recipe);
         } else {
             for (let i = 0; i < ingredients.length; i++) {
-                let ing = ingredients[i].ingredient.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "");
+                let ing = completeNormalize(ingredients[i].ingredient);
                 if (ing.includes(searchbarValue)) {
                     filterTags(recipe);
+                    break;
                 }
             }
         }
