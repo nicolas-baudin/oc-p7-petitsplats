ca781330869f9a04e83ff3df2fa8fe54a7dfabaf
Nicolas Baudin <baudin.nicolas@pm.me>
2023-05-19 16:03:47 +0200

    Missing semicolon

--- scripts/utils/searchengine.js
+++ scripts/utils/searchengine.js
@@ -6,7 +6,7 @@ function searchEngine() {
     } else {
         const nameFilter = recipes.filter(function(r) { return completeNormalize(r.name).includes(searchbarValue); });
         const descFilter = recipes.filter(function(r) { return completeNormalize(r.description).includes(searchbarValue); });
-        const ingFilter = recipes.filter(function(r) { return r.ingredients.some(i => completeNormalize(i.ingredient).includes(searchbarValue))});
+        const ingFilter = recipes.filter(function(r) { return r.ingredients.some(i => completeNormalize(i.ingredient).includes(searchbarValue)); });
         searchbarRecipes = [...new Set([...nameFilter,...descFilter,...ingFilter])];
     }
     allTags = ingTags.concat(appTags, uteTags);
